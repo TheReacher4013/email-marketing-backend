69079c7d19fc935b92c3604fd62833308120e69d
Soni2912@ <bansodesony48@gmail.com>
2026-03-25 12:22:10 +0530

    created authController and and rolemiddleware

---
+++ middleware/rolemiddleware.js
@@ -0,0 +1,30 @@
+const { sendError } = require('../utils/responseHelper');
+
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return sendError(res, 'Not authenticated.', 401);
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return sendError(
+        res,
+        `Access denied. Required role: ${roles.join(' or ')}`,
+        403
+      );
+    }
+
+    next();
+  };
+};
+
+
+const ROLES = {
+  SUPER_ADMIN: 'super_admin',
+  BUSINESS_ADMIN: 'business_admin',
+  MARKETING_MANAGER: 'marketing_manager',
+  VIEWER: 'viewer',
+};
+
+module.exports = { authorize, ROLES };
